f810eb95ae09989a5ecea8bedace4f4f36870098
MiriRCNSE <43878357+MiriRCNSE@users.noreply.github.com>
2018-11-21 21:30:20 +0100

    SpeechStrings.java wird aktualisiert
    Die Welcome-Message wird an den Prototypen angepasst.

--- src/main/java/beesafe/SpeechStrings.java
+++ src/main/java/beesafe/SpeechStrings.java
@@ -1,12 +1,13 @@
 package main.java.beesafe;
 
 public class SpeechStrings {
-	public static final String welcomeMessage = "Hallo <break time =\"1s\"/>. Willkommen bei AskGrandma BeeSafe.";
+	public static final String welcomeMessage = "Willkommen bei Ask Grandma! " 
+												+ "Hallo! Wie kann ich dir helfen?";
 	public static final String welcomeMessage_Reprompt = "Hey ist da jemand <break time =\"1s\"/>? Deine Großmutter hat gerade Zeit für dich.";
 	
 	public static final String stopMessage = "Ich hoffe ich konnte dir helfen. Bis zum nächsten mal!";
 	
-	public static final String BeeSafeName = "AskGrandma - BeeSafe";
+	public static final String BeeSafeName = "bee safe";
 	
 	public static final String helpMessage = "Sag mir, welche Verletzung du hast."; 
 	public static final String helpMessage_Reprompt = "Du kannst mir sagen, welche Verletzung du hast.";
