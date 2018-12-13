a8fa311a0709c30c270aa36b27863c837206fb35
Sebastian <43878791+WasIsDaLos@users.noreply.github.com>
2018-12-13 21:32:39 +0100

    tests für modellklassen erweitert
    musste testklassen umbennen, da sonst immer nur ein test lief. war sehr kurios

--- src/test/java/EmergencyAlternativeTest.java
+++ src/test/java/EmergencyAlternativeTest.java
@@ -6,19 +6,28 @@ import org.junit.Test;
 
 import main.java.beesafe.model.Emergency;
 
-public class EmergencyTest {
+public class EmergencyAlternativeTest {
 
 	@Test
-	public void test() {
+	public void testGetAddress() {
 		Emergency notaufnahme1 = new Emergency("Klinikum rechts der Isar <<break time=\\\"0.4s\\\"/>, "
 				+ "Ismaninger Strasse 22 <break time=\\\"0.4s\\\"/>, 81675 München");
 		Emergency notaufnahme2 = new Emergency("Notfallzentrum - Klinikum Schwabing <break time=\\\"0.4s\\\"/>,"
 				+ "Kölner Platz 1 <break time=\\\"0.4s\\\"/>, 80804 München");
 		assertEquals("Klinikum rechts der Isar <<break time=\\\"0.4s\\\"/>, "
-				+ "Ismaninger Strasse 22 <break time=\\\"0.4s\\\"/>, 81675 München", notaufnahme1.getAddress());
-		assertEquals(112, notaufnahme1.getEmergencyCall());
+				+ "Ismaninger Strasse 22 <break time=\\\"0.4s\\\"/>, 81675 München", notaufnahme1.getAddress());		
 		assertEquals("Notfallzentrum - Klinikum Schwabing <break time=\\\"0.4s\\\"/>,"
 				+ "Kölner Platz 1 <break time=\\\"0.4s\\\"/>, 80804 München", notaufnahme2.getAddress());
 	}
+	
+	@Test
+	public void testGetEmergencyCall() {
+		assertEquals(112, new Emergency("").getEmergencyCall());
+	}
 
+	@Test
+	public void testToString() {
+		Emergency notaufnahme1 = new Emergency("Krankenhaus Buxdehude");
+		assertEquals("Notrufnummer: 112\nAdresse: Krankenhaus Buxdehude", notaufnahme1.toString());
+	}
 }
--- src/test/java/InjuryAlternativeTest.java
+++ src/test/java/InjuryAlternativeTest.java
@@ -1,6 +1,8 @@
 package test.java;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -10,7 +12,7 @@ import main.java.beesafe.model.HouseholdRemedy;
 import main.java.beesafe.model.Injury;
 import main.java.beesafe.model.Risk;
 
-public class InjuryTest {
+public class InjuryAlternativeTest {
 
 	@Test
 	public void test() {
@@ -36,4 +38,26 @@ public class InjuryTest {
 		assertEquals("Stich", bienenstich.getInjury());
 	}
 
+	@Test
+	public void testToString() {
+		ArrayList<HouseholdRemedy> households = new ArrayList<>();
+		households.add(new HouseholdRemedy("bla", "blub"));
+		ArrayList<Risk> risks = new ArrayList<>();	
+		risks.add(new Risk("risk"));
+		Injury stich = new Injury("bienenstich");
+		stich.setHouseholds(households);
+		stich.setRisks(risks);
+		assertEquals("Name: bienenstich\nRisikofaktoren: \nName: risk\nAufgetreten: false\n"+
+				"Hausmittel: \nName: bla\nVorraetig: false\nAnwendung: blub\n",stich.toString());
+	}
+	
+	@Test
+	public void testIsValidTrue() {
+		assertTrue(new Injury("a").isValid());
+	}
+	
+	@Test
+	public void testIsValidFalse() {
+		assertFalse(new Injury("").isValid());
+	}
 }
